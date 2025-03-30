2a176391ae287c58583d8fdd54d1ab1a5147abb9
doge <doge@gmail.com>
2025-03-30 17:43:40 +0800

    233

--- app/configs.tsx
+++ app/configs.tsx
@@ -4,7 +4,7 @@ export const configs = {
     { name: "物品管理", icon: "/item-management.svg", href: "/thing", color: "#2196F3", size: "large", colSpan: 3, rowSpan: 1 },
     { name: "实验室", icon: "/laboratory.svg", href: "/lab", color: "#388e3c", size: "medium", colSpan: 1, rowSpan: 2 },
     { name: "文件", icon: "/todo.svg", href: "/file", color: "#FF5722", size: "medium", colSpan: 2, rowSpan: 1 },
-    { name: "Minecraft", icon: "/minecraft.svg", href: "/mc", color: "#8B5A2B", size: "medium", colSpan: 2, rowSpan: 1 },
+    { name: "工具", icon: "/minecraft.svg", href: "/tool", color: "#8B5A2B", size: "medium", colSpan: 2, rowSpan: 1 },
     { name: "导航", icon: "/navigation.svg", href: "/nav", color: "#FFA000", size: "small", colSpan: 1, rowSpan: 1 },
     { name: "笔记", icon: "/wiki.svg", href: "/note", color: "#1976D2", size: "small", colSpan: 1, rowSpan: 1 },
     { name: "游戏", icon: "/game.svg", href: "/game", color: "#424242", size: "small", colSpan: 1, rowSpan: 1 },
