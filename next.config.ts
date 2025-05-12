a69a83402bd5f43da19aa0946a17c6453ce1ffca
doge <doge@gmail.com>
2025-05-12 21:32:44 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['127.0.0.1', 'localhost', 'next-api.dogeow.com'],
   },
