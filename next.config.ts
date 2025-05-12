c651bbbd82ac8ff588c726103244d897d4ba75b4
doge <doge@gmail.com>
2025-05-12 20:37:47 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
-    domains: ['next-api.dogeow.com'],
+    domains: ['127.0.0.1', 'localhost', 'next-api.dogeow.com'],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          { key: "Cross-Origin-Resource-Policy", value: "cross-origin" }
-        ]
-      }
-    ]
-  }
 };
 
 export default nextConfig;
