c9fc1a35a19e9f0383e00a41cbe8dd3ea8e58d23
doge <doge@gmail.com>
2025-05-22 00:29:25 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   },
   images: {
     domains: ['127.0.0.1', 'localhost', 'next-api.dogeow.com'],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 4096, 5120],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 768, 1024],
   },
 };
 
