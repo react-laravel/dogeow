aa52c2d0bcfae5e8e0d80b010a0e8e2438c547c4
doge <doge@gmail.com>
2025-05-22 00:14:31 +0800

    Update page.tsx

--- app/thing/[id]/page.tsx
+++ app/thing/[id]/page.tsx
@@ -187,7 +187,6 @@ export default function ItemDetail() {
                         alt={item.name}
                         fill
                         className="object-contain"
-                        unoptimized={true}
                       />
                     </div>
                     
