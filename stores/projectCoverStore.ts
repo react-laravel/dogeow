1af91cc401b73984425be1ce84b3cc26d5b4b5e4
doge <doge@gmail.com>
2025-06-06 10:37:01 +0800

    233

---
+++ stores/projectCoverStore.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface ProjectCoverState {
+  showProjectCovers: boolean
+  setShowProjectCovers: (show: boolean) => void
+}
+
+export const useProjectCoverStore = create<ProjectCoverState>()(
+  persist(
+    (set) => ({
+      showProjectCovers: false,
+      setShowProjectCovers: (show: boolean) => set({ showProjectCovers: show }),
+    }),
+    {
+      name: 'project-cover-storage',
+    }
+  )
+) 
