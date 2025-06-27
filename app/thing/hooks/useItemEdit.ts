fba346cf0d935fdefe1500660fe4b30056af2578
doge <doge@gmail.com>
2025-06-28 01:11:42 +0800

    Update useItemEdit.ts

--- app/thing/hooks/useItemEdit.ts
+++ app/thing/hooks/useItemEdit.ts
@@ -50,6 +50,7 @@ export function useItemEdit() {
   }
 
   // 自动保存逻辑
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleAutoSave = useCallback(async (_data: AutoSaveData) => {
     const updateData: Parameters<typeof updateItem>[1] = {
       ...formData,
