0ebfc2f56361c79417ffe1800341f9e1f95ccabb
doge <lky216@gmail.com>
2026-03-08 09:32:09 +0800

    Update configs.tsx

--- app/configs.tsx
+++ app/configs.tsx
@@ -197,7 +197,7 @@ const createModule = (id: string, nameKey: string, descriptionKey: string, url: 
 const SYSTEM_BACKGROUNDS = [
   { id: 'none', nameKey: 'background.none', url: '' },
   { id: 'bg1', nameKey: 'background.bg1', url: '君の名は.webp' },
-  { id: 'bg2', nameKey: 'background.bg12', url: '钢铁侠.jpg' },
+  { id: 'bg2', nameKey: 'background.bg2', url: '钢铁侠.jpg' },
   { id: 'bg3', nameKey: 'background.bg3', url: '速度生活.jpg' },
   { id: 'bg4', nameKey: 'background.bg4', url: '中世纪-骑士.jpeg' },
   { id: 'bg5', nameKey: 'background.bg5', url: '冰与火之歌.png' },
