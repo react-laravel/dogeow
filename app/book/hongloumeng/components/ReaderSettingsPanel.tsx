dd5f7d798ed43fae01a2ba7a96a597352d8980da
doge <doge@gmail.com>
2026-06-20 14:34:37 +0800

    调浅阅读设置遮罩

--- app/book/hongloumeng/components/ReaderSettingsPanel.tsx
+++ app/book/hongloumeng/components/ReaderSettingsPanel.tsx
@@ -45,6 +45,7 @@ export function ReaderSettingsPanel({
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent
         side="right"
+        overlayClassName="bg-black/10"
         className="flex w-1/2 max-w-none flex-col gap-5 px-5 pt-5 pb-6 sm:max-w-none"
       >
         <SheetHeader className="p-0 pr-10">
